f0ce702e87c609331cc5b363b370fb07d8da74fd
Parul Gupta <parulguptasmail@gmail.com>
2018-03-07 19:20:49 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,4 +1,8 @@
 def square_array(array)
   # your code here
-  squared_array =  array.each { |x| x*x } 
+  squared_array = []
+  array.each do |x|
+    squared_array << x*x 
+  end
+  return squared_array
 end
